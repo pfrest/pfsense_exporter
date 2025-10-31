bee4a3f7904302984908e26e0824ea454066664a
Jared Hendrickson <jaredhendrickson13@gmail.com>
2025-10-30 23:52:23 -0600

    chore: clean repo for gh-pages

--- internal/utils/helpers.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-// BoolToFloat64 converts a boolean value to a float64 (1.0 for true, 0.0 for false).
-func BoolToFloat64(b bool) float64 {
-	if b {
-		return 1.0
-	}
-	return 0.0
-}
